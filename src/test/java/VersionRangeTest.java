d1ad268e0d9d8550c1859b78b9c71905b2aa9076
Nils Breunese <nils@breun.nl>
2021-07-12 23:18:03 +0200

    first commit

---
+++ src/test/java/VersionRangeTest.java
@@ -0,0 +1,25 @@
+import org.apache.maven.artifact.versioning.ArtifactVersion;
+import org.apache.maven.artifact.versioning.DefaultArtifactVersion;
+import org.apache.maven.artifact.versioning.VersionRange;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class VersionRangeTest {
+
+    @Test
+    void range_with_single_version_should_contain_that_version() throws Exception {
+        VersionRange range_1_0_0 = VersionRange.createFromVersionSpec("1.0.0");
+        ArtifactVersion version_1_0_0 = new DefaultArtifactVersion("1.0.0");
+
+        assertThat(range_1_0_0.containsVersion(version_1_0_0)).isTrue();
+    }
+
+    @Test
+    void range_with_single_version_should_not_contain_another_version() throws Exception {
+        VersionRange range_1_0_0 = VersionRange.createFromVersionSpec("1.0.0");
+        ArtifactVersion version_2_0_0 = new DefaultArtifactVersion("2.0.0");
+
+        assertThat(range_1_0_0.containsVersion(version_2_0_0)).isFalse();
+    }
+}
