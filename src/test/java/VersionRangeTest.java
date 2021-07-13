c40d9e9ef6fec4b2b52eb25648f7a32069f8607c
Nils Breunese <nils@breun.nl>
2021-07-13 20:10:09 +0200

    Use correct syntax for a closed range containing a single version

--- src/test/java/VersionRangeTest.java
+++ src/test/java/VersionRangeTest.java
@@ -9,7 +9,7 @@ class VersionRangeTest {
 
     @Test
     void range_with_single_version_should_contain_that_version() throws Exception {
-        VersionRange range_1_0_0 = VersionRange.createFromVersionSpec("1.0.0");
+        VersionRange range_1_0_0 = VersionRange.createFromVersionSpec("[1.0.0]");
         ArtifactVersion version_1_0_0 = new DefaultArtifactVersion("1.0.0");
 
         assertThat(range_1_0_0.containsVersion(version_1_0_0)).isTrue();
@@ -17,7 +17,7 @@ class VersionRangeTest {
 
     @Test
     void range_with_single_version_should_not_contain_another_version() throws Exception {
-        VersionRange range_1_0_0 = VersionRange.createFromVersionSpec("1.0.0");
+        VersionRange range_1_0_0 = VersionRange.createFromVersionSpec("[1.0.0]");
         ArtifactVersion version_2_0_0 = new DefaultArtifactVersion("2.0.0");
 
         assertThat(range_1_0_0.containsVersion(version_2_0_0)).isFalse();
